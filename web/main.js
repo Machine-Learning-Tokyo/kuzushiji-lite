5cdf7abdc9e52258ef5fc5a4b9e805eef088c8a5
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-02-16 16:37:41 +0900

    More plus camera (#3)
    * red chars
    
    * camera

--- web/main.js
+++ web/main.js
@@ -11,13 +11,72 @@ class Main {
     this.contentImg = document.getElementById('content-img');
     this.uploadBtn = document.getElementById('upload-button');
     this.processBtn = document.getElementById('process-button');
+    this.takePicBtn = document.getElementById('take-pic-button');
 
     this.contentCanvas = document.getElementById('content-canvas');
     this.contentCtx = this.contentCanvas.getContext('2d');
 
+    this.initalizeWebcamVariables();
     this.setUploadBtn();
     this.setUpProcessBtn();
     this.drawImageOnCanvas();
+    this.setUpTakePicBtn();
+  }
+
+  setUpTakePicBtn() {
+    this.takePicBtn.onclick = () => {
+      console.log('taking pic');
+      this.openModal();
+    }
+  }
+
+  openModal() {
+    this.camModal.modal('show');
+    this.snapButton.onclick = () => {
+      const hiddenCanvas = document.getElementById('hidden-canvas');
+      const hiddenContext = hiddenCanvas.getContext('2d');
+      hiddenCanvas.width = this.webcamVideoElement.width;
+      hiddenCanvas.height = this.webcamVideoElement.height;
+      hiddenContext.drawImage(this.webcamVideoElement, 0, 0, 
+        hiddenCanvas.width, hiddenCanvas.height);
+      const imageDataURL = hiddenCanvas.toDataURL('image/jpg');
+      this.contentImg.src = imageDataURL;
+      this.contentImg.onload = () => {
+        this.drawImageOnCanvas();
+      }
+      this.camModal.modal('hide');
+    };
+  }
+
+  initalizeWebcamVariables() {
+    this.camModal = $('#cam-modal');
+
+    this.snapButton = document.getElementById('snap-button');
+    this.webcamVideoElement = document.getElementById('webcam-video');
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+    this.camModal.on('hidden.bs.modal', () => {
+      this.stream.getTracks()[0].stop();
+    })
+
+    this.camModal.on('shown.bs.modal', () => {
+      navigator.getUserMedia(
+        {
+          video: true
+        },
+        (stream) => {
+          this.stream = stream;
+          this.webcamVideoElement.srcObject = stream;
+          this.webcamVideoElement.play();
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    })
   }
 
   drawImageOnCanvas() {
@@ -37,14 +96,19 @@ class Main {
 
   setUpProcessBtn() {
     this.processBtn.onclick = () => {
+      this.processBtn.disabled = true;
       console.log('processing');
+      this.contentCtx.font = "20px Georgia";
+      this.contentCtx.fillStyle = "red";
       this.contentCtx.strokeStyle = 'red';
       const segments = this.segmentImage();
       for (let i=0; i < segments.length; i++) {
         let s = segments[i];
         this.contentCtx.rect(s.x, s.y, s.width, s.height);
+        this.contentCtx.fillText(s.char, s.x, s.y+15)
       }
       this.contentCtx.stroke();
+      this.processBtn.disabled = false;
     }
   }
 
@@ -55,18 +119,21 @@ class Main {
         'y': 200,
         'width': 20,
         'height': 20,
+        'char': "\u3041"
       },
       {
         'x': 160,
         'y': 215,
         'width': 25,
         'height': 25,
+        'char': "\u3045"
       },
       {
         'x': 30,
         'y': 400,
         'width': 32,
         'height': 32,
+        'char': "\u3043"
       },
     ]
   }
